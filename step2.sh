da6efe2acf9d8c036a8048e4919389f771803b93
Dmitriy Krasnikov <testino@outlook.com>
2014-12-04 16:48:13 +0200

    fix rpmbuild errors

--- step2.sh
+++ step2.sh
@@ -6,29 +6,34 @@ cp mc-installer/spec/* ~/rpmbuild/SPECS
 cp mc-installer/patch/* ~/rpmbuild/SOURCES
 cd ~/rpmbuild/SOURCES
 if [ ! -f "autoconf-2.69.tar.xz" ]
-then
-wget http://ftp.gnu.org.ua/gnu/autoconf/autoconf-2.69.tar.xz
+	then
+	wget http://ftp.gnu.org.ua/gnu/autoconf/autoconf-2.69.tar.xz
 fi
 if [ ! -f "gflags-2.1.1.tar.gz" ]
-then
-wget http://github.com/schuhschuh/gflags/archive/v2.1.1/gflags-2.1.1.tar.gz
+	then
+	wget http://github.com/schuhschuh/gflags/archive/v2.1.1/gflags-2.1.1.tar.gz
 fi
 if [ ! -f "glog-0.3.3.tar.gz" ]
-then
-wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+	then
+	wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
 fi
 if [ ! -f "ragel-6.9.tar.gz" ]
-then
-wget http://www.colm.net/files/ragel/ragel-6.9.tar.gz
+	then
+	wget http://www.colm.net/files/ragel/ragel-6.9.tar.gz
 fi
 if [ ! -f "double-conversion-master.tar.gz" ]
-then
-wget https://codeload.github.com/floitsch/double-conversion/tar.gz/master -O double-conversion-master.tar.gz
+	then
+	wget https://codeload.github.com/floitsch/double-conversion/tar.gz/master -O double-conversion-master.tar.gz
 fi
-rpmbuild -bb ~/rpmbuild/SPECS/* 
+rpmbuild -bb ~/rpmbuild/SPECS/autoconf.spec
+rpmbuild -bb ~/rpmbuild/SPECS/double-conversion.spec
+rpmbuild -bb ~/rpmbuild/SPECS/gflags.spec
+rpmbuild -bb  ~/rpmbuild/SPECS/ragel.spec
 cd ~/rpmbuild/RPMS/x86_64/
-rpm -ivh double-conversion-devel-master-1.el6.x86_64.rpm double-conversion-master-1.el6.x86_64.rpm gflags-2.1.1-6.el6.x86_64.rpm gflags-devel-2.1.1-6.el6.x86_64.rpm glog-0.3.3-8.el6.x86_64.rpm glog-devel-0.3.3-8.el6.x86_64.rpm ragel-6.9-2.3.x86_64.rpm 
+rpm -ivh double-conversion-devel-master-1.el6.x86_64.rpm double-conversion-master-1.el6.x86_64.rpm gflags-2.1.1-6.el6.x86_64.rpm gflags-devel-2.1.1-6.el6.x86_64.rpm ragel-6.9-2.3.x86_64.rpm 
 rpm -Uvh ~/rpmbuild/RPMS/noarch/autoconf-2.69-12.2.noarch.rpm
+rpmbuild -bb ~/rpmbuild/SPECS/glog.spec
+rpm -ivh glog-0.3.3-8.el6.x86_64.rpm glog-devel-0.3.3-8.el6.x86_64.rpm
 echo '/usr/local/lib' >> /etc/ld.so.conf.d/libs.conf && ldconfig
 cd /opt && git clone https://github.com/facebook/folly.git
 cd /opt/folly/folly/ && autoreconf -ivf
