989c5d5e91cfcdffbb470f3e6cf72d0d7b4aa151
Dmitriy Krasnikov <testino@outlook.com>
2014-12-04 16:50:20 +0200

    fix ragel deps miss

--- step1.sh
+++ step1.sh
@@ -2,7 +2,7 @@
 yum -y update 
 wget http://www.hop5.in/yum/el6/hop5.repo -P /etc/yum.repos.d/
 yum -y groupinstall 'Development Tools'
-yum -y install unzip bzip2-devel libtool libevent-devel libcap-devel openssl-devel bison flex snappy-devel numactl-devel cyrus-sasl-devel boost-devel python-setuptools cmake
+yum -y install unzip bzip2-devel libtool libevent-devel libcap-devel openssl-devel bison flex snappy-devel numactl-devel cyrus-sasl-devel boost-devel python-setuptools cmake transfig tetex-latex gcc-objc
 rpm -Uvh http://sourceforge.net/projects/scons/files/scons/2.3.4/scons-2.3.4-1.noarch.rpm
 yum -y install centos-release-SCL
 yum -y install python27
