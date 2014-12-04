de941db6ea0185c4935cd1682a0371f7384fd676
Dmitriy Krasnikov <testino@outlook.com>
2014-12-04 16:21:53 +0200

    change repo priority

--- step1.sh
+++ step1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum -y groupinstall 'Development Tools'
-wget http://www.hop5.in/yum/el6/hop5.repo -P /etc/yum.repos.d/
 yum -y update 
+wget http://www.hop5.in/yum/el6/hop5.repo -P /etc/yum.repos.d/
+yum -y groupinstall 'Development Tools'
 yum -y install unzip bzip2-devel libtool libevent-devel libcap-devel openssl-devel bison flex snappy-devel numactl-devel cyrus-sasl-devel boost-devel python-setuptools cmake
 rpm -Uvh http://sourceforge.net/projects/scons/files/scons/2.3.4/scons-2.3.4-1.noarch.rpm
 yum -y install centos-release-SCL
