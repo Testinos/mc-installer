e37065001cd0e1a7408c5f312c2b314e14775661
Dmitriy Krasnikov <testino@outlook.com>
2014-12-04 17:57:09 +0200

    fix build errors

--- step2.sh
+++ step2.sh
@@ -21,10 +21,6 @@ if [ ! -f "ragel-6.9.tar.gz" ]
 	then
 	wget http://www.colm.net/files/ragel/ragel-6.9.tar.gz
 fi
-if [ ! -f "double-conversion-master.tar.gz" ]
-	then
-	wget https://codeload.github.com/floitsch/double-conversion/tar.gz/master -O double-conversion-master.tar.gz
-fi
 rpmbuild -bb ~/rpmbuild/SPECS/autoconf.spec
 rpmbuild -bb ~/rpmbuild/SPECS/double-conversion.spec
 rpmbuild -bb ~/rpmbuild/SPECS/gflags.spec
@@ -34,8 +30,12 @@ rpm -ivh double-conversion-devel-master-1.el6.x86_64.rpm double-conversion-maste
 rpm -Uvh ~/rpmbuild/RPMS/noarch/autoconf-2.69-12.2.noarch.rpm
 rpmbuild -bb ~/rpmbuild/SPECS/glog.spec
 rpm -ivh glog-0.3.3-8.el6.x86_64.rpm glog-devel-0.3.3-8.el6.x86_64.rpm
-echo '/usr/local/lib' >> /etc/ld.so.conf.d/libs.conf && ldconfig
+cd /opt 
+git clone https://github.com/floitsch/double-conversion.git
+cd double-conversion && scons install 
+ln -sf src double-conversion && scons -f SConstruct
 
+echo '/usr/local/lib' >> /etc/ld.so.conf.d/libs.conf && ldconfig
 
 cd /opt && git clone https://github.com/facebook/folly.git
 export LD_LIBRARY_PATH="/opt/folly/folly/lib:$LD_LIBRARY_PATH"
